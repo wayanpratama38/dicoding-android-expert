6fd85b13140373025371d58163e46fc6fcbd9a02
wynprtm <wayanpratama38@gmail.com>
2025-02-08 17:57:59 +0700

    Doing Repository and Use Case Code Lab

--- CleanArchitecture/StarterProject/app/src/main/java/com/dicoding/tourismapp/core/di/Injection.kt
+++ CleanArchitecture/StarterProject/app/src/main/java/com/dicoding/tourismapp/core/di/Injection.kt
@@ -7,11 +7,23 @@ import com.dicoding.tourismapp.core.data.source.local.room.TourismDatabase
 
 import com.dicoding.tourismapp.core.data.TourismRepository
 import com.dicoding.tourismapp.core.data.source.remote.RemoteDataSource
+import com.dicoding.tourismapp.core.domain.repository.ITourismRepository
+import com.dicoding.tourismapp.core.domain.usecase.TourismInteractor
+import com.dicoding.tourismapp.core.domain.usecase.TourismUseCase
 import com.dicoding.tourismapp.core.utils.AppExecutors
 import com.dicoding.tourismapp.core.utils.JsonHelper
 
 object Injection {
-    fun provideRepository(context: Context): TourismRepository {
+//    private fun provideRepository(context: Context): ITourismRepository {
+//        val database = TourismDatabase.getInstance(context)
+//
+//        val remoteDataSource = RemoteDataSource.getInstance(JsonHelper(context))
+//        val localDataSource = LocalDataSource.getInstance(database.tourismDao())
+//        val appExecutors = AppExecutors()
+//
+//        return TourismRepository.getInstance(remoteDataSource, localDataSource, appExecutors)
+//    }
+    private fun provideRepository(context: Context): ITourismRepository {
         val database = TourismDatabase.getInstance(context)
 
         val remoteDataSource = RemoteDataSource.getInstance(JsonHelper(context))
@@ -19,5 +31,10 @@ object Injection {
         val appExecutors = AppExecutors()
 
         return TourismRepository.getInstance(remoteDataSource, localDataSource, appExecutors)
+    }
+
+    fun provideTourismUseCase(context: Context) : TourismUseCase{
+        val repository = provideRepository(context)
+        return TourismInteractor(repository)
     }
 }
